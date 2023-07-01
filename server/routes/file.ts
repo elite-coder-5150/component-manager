202c0fc321cbc317b05eaf1e27cadc1ca1cbe8b1
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-30 23:07:46 -0500

    defined the file methods

---
+++ server/routes/file.ts
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+import {FileController} from "../controllers/file.controller";
+const fc = new FileController();
+router.get('/', );
